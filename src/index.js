6b4dfc3664f1a3df8fe9f5af847a016fded825f3
MarkBettley <108556195+MarkBettley@users.noreply.github.com>
2026-04-13 00:48:25 -0600

    Revert "Feature/styled components"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import './index.css';
 import App from './App';
->>>>>>> 504fc845fbb758d39dfe377f73f93bb17b39bace
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-
-reportWebVitals();
-=======
-);
->>>>>>> 504fc845fbb758d39dfe377f73f93bb17b39bace
